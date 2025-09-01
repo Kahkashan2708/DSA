8b2071810e298e656c002aaf65635a415643a85f
Kahkashan Manzoor <ma23c014@smail.iitm.ac.in>
2025-09-01 23:48:39 +0530

    Time: 9 ms (62.12%) | Memory: 12.4 MB (51.95%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,9 +1,8 @@
 class Solution(object):
     def isPalindrome(self, x):
-        
+
         if x < 0:
             return False
-        
-        # Convert number to string and check if it reads same backwards
+    
         return str(x) == str(x)[::-1]
         
