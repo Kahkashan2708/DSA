5103dcb5c2859ec8d4a9683dae29debfa920b596
Kahkashan Manzoor <ma23c014@smail.iitm.ac.in>
2025-09-10 15:06:56 +0530

    Time: 3 ms (59.39%) | Memory: 12.5 MB (86.59%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isValid(self, s):
+        mapping = {')':'(', '}': '{', ']': '[' }
+        stack = []
+
+        for i in s:
+            if i in mapping:
+                if stack and stack[-1] == mapping[i]:
+                    stack.pop()
+                else:    
+                    return False
+            else:
+                stack.append(i)
+                
+        return not stack                
+        
