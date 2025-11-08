a49f12249ad35a3fcad3bd9fefe2e7d335ae075c
Kahkashan Manzoor <ma23c014@smail.iitm.ac.in>
2025-11-08 23:19:50 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (62.00%) - LeetSync

---
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def preorderTraversal(self, root):
+         
+        lst = []
+        def dfs(node):
+            if not node:
+                return None
+            lst.append(node.val)   
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        return lst     
+        
